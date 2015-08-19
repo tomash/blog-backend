68257e816bda1f59e5f087ac87b40b37bc3244f6
Wojtek Majewski <jumski@gmail.com>
2015-08-19 15:33:27 +0200

    Make posts route fetch all posts via xhr

--- emberapp/app/routes/posts.js
+++ emberapp/app/routes/posts.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      {body: 'abc', title: 'omg'},
-      {body: 'xkc', title: 'hax'}
-    ];
+    return this.store.findAll('post');
   },
   setupController(controller, posts) {
     console.log("the posts are ", posts);

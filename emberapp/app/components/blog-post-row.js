be468cc0691671bfa0438d32fa952a9dded43d93
Wojtek Majewski <jumski@gmail.com>
2015-08-20 12:09:25 +0200

    Ditch controller in favour of blog-post-row component

--- emberapp/app/components/blog-post-row.js
+++ emberapp/app/components/blog-post-row.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.Component.extend({
+  tagName: 'tr',
   actions: {
-    destroy(post) {
+    destroy() {
+      let post = this.get('post');
+
       if (confirm(`Do you really want to destroy post ${post.title}?`)) {
         post.destroyRecord();
       }

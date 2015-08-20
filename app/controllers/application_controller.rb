61b5cea87ba1aebd77cb89a9ecae6f738212263a
Wojtek Majewski <jumski@gmail.com>
2015-08-20 11:00:42 +0200

    Use http auth and disable csrf token verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # this app is API only, do not verify csrf token
+  protect_from_forgery with: :null_session
 
+  http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show]
+
+  # We want Content-Type that Ember's adapters use to be recognized as :json
   before_action :determine_format
 
   def determine_format

388a0a67ecb92fc877e4f6d19bd1f4140f72325f
Wojtek Majewski <jumski@gmail.com>
2015-08-20 16:46:52 +0200

    Handle creating records in posts/index route

--- emberapp/app/routes/posts/index.js
+++ emberapp/app/routes/posts/index.js
@@ -6,5 +6,14 @@ export default Ember.Route.extend({
   },
   setupController(controller, posts) {
     controller.set('posts', posts);
+  },
+
+  actions: {
+    createPost(post) {
+      console.log('posts-index-route:createPost', post);
+
+      let store = this.get('store');
+      store.createRecord('post', post);
+    }
   }
 });

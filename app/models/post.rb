8cc05084296a16936f068d9ede3143a6c49da349
Wojtek Majewski <jumski@gmail.com>
2015-08-21 17:17:55 +0200

    Scaffold comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  validates_presence_of :body, :title
-
+  validates :body, :title, presence: true
   validates :title, uniqueness: true
   validates :body, length: { minimum: 100 }
+
+  has_many :comments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 Post.destroy_all
+Comment.destroy_all
+
+def lorem(num)
+  'Lorem ipsum dolor sit amet. '*num
+end
 
 5.times do |n|
-  Post.create title: "Post title ##{n}", body: 'Lorem ipsum dolor sit amet. '*10
+  post = Post.create! title: "Post title ##{n}", body: lorem(10)
+
+  rand(0..6).times do |n|
+    Comment.create! body: lorem(3), post: post
+  end
 end

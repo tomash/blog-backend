51255c776f8a189658786b6031d4898d8ae6335c
Wojtek Majewski <jumski@gmail.com>
2015-08-24 09:03:47 +0200

    Add Comment model

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
+  embed :ids, embed_in_root: true
+
   attributes :id, :body
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
+  embed :ids, embed_in_root: true
+
   attributes :id, :title, :body, :created_at, :updated_at
 
   has_many :comments

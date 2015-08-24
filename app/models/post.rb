be022f1bdf32f8d94416a6a5db5ff21d56ba7641
Wojtek Majewski <jumski@gmail.com>
2015-08-24 09:09:25 +0200

    Lower body limit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :body, :title, presence: true
   validates :title, uniqueness: true
-  validates :body, length: { minimum: 100 }
+  validates :body, length: { minimum: 20 }
 
   has_many :comments
 end

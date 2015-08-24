ee6981c874fd80e6e1e30e71cbb5c96078c45cfd
Wojtek Majewski <jumski@gmail.com>
2015-08-24 09:05:45 +0200

    Make posts_controller respond only to json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html, :json
+  respond_to :json
 
   def index
     @posts = Post.includes(:comments)

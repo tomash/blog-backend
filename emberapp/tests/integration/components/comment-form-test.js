35432c0a6944d48c197190a672569a3347c6b33f
Wojtek Majewski <jumski@gmail.com>
2015-08-25 15:34:00 +0200

    Rename new-comment-form to comment-form

--- emberapp/tests/integration/components/comment-form-test.js
+++ emberapp/tests/integration/components/comment-form-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('new-comment-form', 'Integration | Component | new comment form', {
+moduleForComponent('comment-form', 'Integration | Component | new comment form', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{new-comment-form}}`);
+  this.render(hbs`{{comment-form}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#new-comment-form}}
+    {{#comment-form}}
       template block text
-    {{/new-comment-form}}
+    {{/comment-form}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');

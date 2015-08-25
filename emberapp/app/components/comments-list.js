32631e4684594147ae8eab80e9b6d0ae77b92531
Wojtek Majewski <jumski@gmail.com>
2015-08-25 12:55:35 +0200

    [wip] show only persisted comments
    this is not yet working, it updates the computed properties after next
    comment is populated

--- emberapp/app/components/comments-list.js
+++ emberapp/app/components/comments-list.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   /*
+   * Show only comments that are saved
+   */
+  filteredComments: Ember.computed.filter('comments', function(comment) {
+    return !comment.get('isNew');
+  }),
+
+  /*
    * Sort comments by createdAt in descending order
    */
-  orderedComments: Ember.computed.sort('comments', function(commentA, commentB) {
+  orderedComments: Ember.computed.sort('filteredComments', function(commentA, commentB) {
     let timeA = commentA.get('createdAt').getTime(),
         timeB = commentB.get('createdAt').getTime();
 

4177474692c0dc0bf508b985c11306d0a1aa6d6e
Wojtek Majewski <jumski@gmail.com>
2015-08-25 11:19:37 +0200

    Add comment-form

--- emberapp/app/components/new-comment-form.js
+++ emberapp/app/components/new-comment-form.js
@@ -2,12 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'span',
-  isFormShown: false,
   classNames: ['comment-form'],
 
   actions: {
-    toggleForm() {
-      this.toggleProperty('isFormShown');
+    submit() {
     }
   }
 });

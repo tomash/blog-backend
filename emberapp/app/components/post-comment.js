d32ca3cda881579f4d0759855f55849da4655cae
Wojtek Majewski <jumski@gmail.com>
2015-08-26 13:53:56 +0200

    Update post-comment styles a little bit

--- emberapp/app/components/post-comment.js
+++ emberapp/app/components/post-comment.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['post-comment'],
+
   actions: {
     updateComment(comment) {
       this.sendAction('updateComment', comment)
+    },
+
+    destroy(comment) {
+      this.sendAction('destroyComment', comment);
     },
 
     toggle() {

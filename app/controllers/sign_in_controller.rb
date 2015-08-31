865f71b4c9a60426ea7f781eab7a27711423969b
Wojtek Majewski <jumski@gmail.com>
2015-08-31 11:23:31 +0200

    Protect all pages with http auth

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -1,11 +1,9 @@
 class SignInController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "password", only: :validate_http_auth
-
   respond_to :json
 
   def validate_http_auth
-    # only respond status, that will be 401 in case of failed http auth,
-    # is relevant, hence this can render anything really
+    # this is a dummy action, so we have a concrete endpoint to hit
+    # and get either 200 or 401 status
     render nothing: true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
 
+  # this is a dummy route required only for its return status
   post 'sign_in' => 'sign_in#validate_http_auth'
 end

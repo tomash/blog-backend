9d983a1253cf3c698f5091ad8661bff38e7209b6
Wojtek Majewski <jumski@gmail.com>
2015-09-01 15:48:54 +0200

    Enable http auth for other actions, put username and password for all rquests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # this app is API only, do not verify csrf token
   protect_from_forgery with: :null_session
 
-  http_basic_authenticate_with name: "admin", password: "password", only: :validate_http_auth
+  http_basic_authenticate_with name: "admin", password: "password", only: [:edit, :destroy, :validate_http_auth]
 
   # We want Content-Type that Ember's adapters use to be recognized as :json
   before_action :determine_format

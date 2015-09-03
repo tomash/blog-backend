99c80cd640d3ddb92240669798d5970956b8ee3e
Wojtek Majewski <jumski@gmail.com>
2015-09-03 19:22:50 +0200

    Fix User validation bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_secure_token :auth_token
 
-  validates :username, :password, presence: true
+  validates :username, :password_digest, presence: true
 end

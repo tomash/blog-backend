d4acf9c554cdda9ad2368011688ec436e3779aed
Wojtek Majewski <jumski@gmail.com>
2015-09-03 20:19:26 +0200

    Return user data from `/auth_token`

--- app/controllers/auth_tokens_controller.rb
+++ app/controllers/auth_tokens_controller.rb
@@ -1,11 +1,13 @@
 class AuthTokensController < ApplicationController
   skip_before_action :authenticate_by_auth_token
 
+  respond_to :json
+
   def create
     user = User.find_by!(username: params[:username])
 
     if authenticate_by_strategy(user: user, params: params)
-      render json: { auth_token: user.auth_token }
+      respond_with user, location: nil
     else
       render nothing: true, status: 401
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :auth_token
 end

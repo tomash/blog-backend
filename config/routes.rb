9732a450a6b0349cbe77e49a1f603de8c0fa013d
Wojtek Majewski <jumski@gmail.com>
2015-09-04 16:11:57 +0200

    Remove `/current_user` route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'current_user', to: 'users#show_current'
-
   resource :auth_token, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy, :update]
   resources :posts, except: [:new, :edit]

15d65f4be0c65859ba4d6a95d6f178ca9f9804a8
Wojtek Majewski <jumski@gmail.com>
2015-09-04 18:40:30 +0200

    Properly register mime type for `application/vnd.api+json`
    get rid of `ApplicationController#determine_format` hack.
    More info:
    https://github.com/rails-api/active_model_serializers/issues/1027
    
    Really dunno why it was working with `determine_format` until
    `active_model_serializers` were updated to master,
    but not worth digging this on friday evening :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,18 +6,9 @@ class ApplicationController < ActionController::Base
   # this app is API only, do not verify csrf token
   protect_from_forgery with: :null_session
 
-  # We want Content-Type that Ember's adapters use to be recognized as :json
-  before_action :determine_format
-
   private
 
   attr_reader :current_user # override `jump_in` variation that uses session/cookies
-
-  def determine_format
-    if request.format == 'application/vnd.api+json'
-      request.format = :json
-    end
-  end
 
   def authenticate_by_auth_token
     auth_token = request.headers['HTTP_X_TOKEN'].presence
